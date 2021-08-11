6d89864c4d27fbd6a837c06877e87a641bc8be67
Aphro Code <aphrocode9@gmail.com>
2021-08-12 02:04:00 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,70 +1,7 @@
-# Getting Started with Create React App
+# PVU Link Generator
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+**DONATION:**
 
-## Available Scripts
-
-In the project directory, you can run:
-
-### `yarn start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+BNB(BSC Aphrogame) : [0xc3B04a735c5cDea8e19e9858d9E4D916B14605c2](https://bscscan.com/address/0xc3B04a735c5cDea8e19e9858d9E4D916B14605c2)
+BNB(BSC PakPres) : [0xf0F16b80Ff38F882dD5b3C0Ed6ec4e06774324D7](https://bscscan.com/address/0xf0F16b80Ff38F882dD5b3C0Ed6ec4e06774324D7)
+BNB(BSC Altergogi) : [0xEde11088F435e293574Fe87aC254004F0c7a41Aa](https://bscscan.com/address/0xEde11088F435e293574Fe87aC254004F0c7a41Aa)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable array-callback-return */
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import _ from 'lodash'
+import tokenGuide from '../src/token.png'
+import { landAddresses } from './Env'
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => {
+    generateAddress(data)
+  }
+
+  const generateAddress = ({token, page, type}) => {
+    console.log('generating...')
+    
+    landAddresses.map(address => {
+      let config = {
+        method: 'get',
+        url: `https://backend-farm.plantvsundead.com/farms/other/${address}?limit=10&offset=${(page - 1) * 10}`,
+        headers: { 
+          'Authorization': `Bearer ${token}`
+        }
+      }
+      
+      axios(config)
+      .then(function (response) {
+        const { data } = response
+        data?.data.map(plant => {
+          let icon
+          if (plant.hasCrow && plant.waterStatus.needWater) {
+            icon = '🦅💦'
+          } else if (plant.hasCrow) {
+            icon = '🦅'
+          } else {
+            icon ='💦'
+          }
+          if (type === 'crow' && plant.hasCrow) {
+            console.log('%c            ', `font-size:25px; background:url('https://pvuresources.s3.ap-southeast-2.amazonaws.com/icon/plant/16_2.png') no-repeat;background-size: auto 100%`)
+            console.log(`${icon}`, `https://marketplace.plantvsundead.com/farm/other/${address}`, `crow: ${plant.hasCrow}`, `water: ${_.find(plant.activeTools, { name: 'Water' })?.count}`)
+          } else if (type === 'water' && plant.waterStatus.needWater) {
+            console.log('%c            ', `font-size:25px; background:url('https://pvuresources.s3.ap-southeast-2.amazonaws.com/icon/plant/16_2.png') no-repeat;background-size: auto 100%`)
+            console.log(`${icon}`, `https://marketplace.plantvsundead.com/farm/other/${address}`, `crow: ${plant.hasCrow}`, `water: ${_.find(plant.activeTools, { name: 'Water' })?.count}`)
+          } else if (type === 'both' && (plant.waterStatus.needWater || plant.hasCrow)) {
+            console.log('%c            ', `font-size:25px; background:url('https://pvuresources.s3.ap-southeast-2.amazonaws.com/icon/plant/16_2.png') no-repeat;background-size: auto 100%`)
+            console.log(`${icon}`, `https://marketplace.plantvsundead.com/farm/other/${address}`, `crow: ${plant.hasCrow}`, `water: ${_.find(plant.activeTools, { name: 'Water' })?.count}`)
+          } else {
+            console.log('.')
+          }
+        })
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-section">
+      <img className="logo" src="https://pbs.twimg.com/profile_images/1409847793355657219/LQQ989bP_400x400.jpg" alt="logo" />
+      <h1 style={{ textAlign: 'center' }}>PVU Link Generator</h1>
+      <section className="ui container segment">
+        <form className="ui form" onSubmit={handleSubmit(onSubmit)}>
+          <div className="field">
+            <label>Your Token</label>
+            <input type="text" defaultValue="" {...register("token")} required />
+          </div>
+          <div className="field">
+            <label>Page</label>
+            <input type="number" min="1" name="last-name" defaultValue="1" {...register("page")} />
+          </div>
+          <div className="field">
+            <div className="ui radio checkbox">
+              <input id="crow" type="radio" value="crow" {...register("type", {})} />
+              <label htmlFor="crow" style={{ cursor: 'pointer' }}>Crow Only</label>
+            </div>
+          </div>
+          <div className="field">
+            <div className="ui radio checkbox">
+              <input id="water" type="radio" value="water" {...register("type", {})} />
+              <label htmlFor="water" style={{ cursor: 'pointer' }}>Water Only</label>
+            </div>
+          </div>
+          <div className="field">
+            <div className="ui radio checkbox">
+              <input id="both" type="radio" value="both" defaultChecked {...register("type", {})} />
+              <label htmlFor="both" style={{ cursor: 'pointer' }}>Both</label>
+            </div>
+          </div>
+          <button className="ui button primary" type="submit">Generate Link</button>
+          <p style={{ color: '#AAA' }}><small><i>* Check your console after pressing "Generate Link" button</i></small></p>
+        </form>
+        <div className="ui divider" />
+        <h3>Get your token from Local Storage browser:</h3>
+        <img src={tokenGuide} alt="token guide" />
+        <h2>Credits:</h2>
+        <div className="ui relaxed divided list">
+          <div className="item">
+            <i className="large bitcoin middle aligned icon"></i>
+            <div className="content">
+              <span className="header">Aphrogame</span>
+              <div className="description">
+                Donate: <a href="https://bscscan.com/address/0xc3B04a735c5cDea8e19e9858d9E4D916B14605c2" target="_blank">
+                  0xc3B04a735c5cDea8e19e9858d9E4D916B14605c2
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <i className="large bitcoin middle aligned icon"></i>
+            <div className="content">
+              <span className="header">PakPres</span>
+              <div className="description">
+                Donate: <a href="https://bscscan.com/address/0xf0F16b80Ff38F882dD5b3C0Ed6ec4e06774324D7" target="_blank">
+                  0xf0F16b80Ff38F882dD5b3C0Ed6ec4e06774324D7
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <i className="large bitcoin middle aligned icon"></i>
+            <div className="content">
+              <span className="header">alteregogi</span>
+              <div className="description">
+                Donate: <a href="https://bscscan.com/address/0xEde11088F435e293574Fe87aC254004F0c7a41Aa" target="_blank">
+                  0xEde11088F435e293574Fe87aC254004F0c7a41Aa
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
